2e57fcbb74cf5a8df781a00f156f5e9fd9ac79c4
IAmMoltony <koronavirusnyj@gmail.com>
2024-02-09 15:05:10 +0600

    Add main mebnu

--- src/NonoSharp.cs
+++ src/NonoSharp.cs
@@ -6,6 +6,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace NonoSharp;
 
+public enum GameState
+{
+    Game,
+    MainMenu
+}
+
 public class NonoSharpGame : Game
 {
     private GraphicsDeviceManager _graphics;
@@ -14,6 +20,13 @@ public class NonoSharpGame : Game
     private MouseState _mouse;
     private MouseState _mouseOld;
     private FPSCounter _fpsCounter;
+    private GameState _state;
+    private int _mainMenuSelect;
+
+    private static string[] _mainMenuButtons = {
+        "Play",
+        "Quit"
+    };
 
     private static float _solveTime = 0;
     private static Thread _solveTimeThread;
@@ -32,6 +45,8 @@ public class NonoSharpGame : Game
 
     public NonoSharpGame()
     {
+        _state = GameState.MainMenu;
+        _mainMenuSelect = -1;
         _fpsCounter = new();
         _solveTime = 0;
         _board = new ("Levels/TestLevel.nono");
@@ -49,6 +64,7 @@ public class NonoSharpGame : Game
         _graphics.ApplyChanges();
         Window.AllowUserResizing = true;
 
+        _solveTimeTick = false;
         _solveTimeThread.Start();
 
         base.Initialize();
@@ -66,14 +82,39 @@ public class NonoSharpGame : Game
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
         _mouseOld = _mouse;
         _mouse = Mouse.GetState();
-        _board.Update(_mouse, _mouseOld, GraphicsDevice);
-        if (_board.IsSolved)
-            _solveTimeTick = false;
+
+        switch (_state)
+        {
+        case GameState.Game:
+            _board.Update(_mouse, _mouseOld, GraphicsDevice);
+            if (_board.IsSolved)
+                _solveTimeTick = false;
+            break;
+        case GameState.MainMenu:
+            if (_mouse.X >= 10 && _mouse.Y >= 100 && _mouse.X <= 70 && _mouse.Y <= 129)
+                _mainMenuSelect = 0;
+            else if (_mouse.X >= 10 && _mouse.Y >= 124 && _mouse.X <= 70 && _mouse.Y <= 158)
+                _mainMenuSelect = 1;
+            else
+                _mainMenuSelect = -1;
+
+            if (_mouseOld.LeftButton == ButtonState.Released && _mouse.LeftButton == ButtonState.Pressed)
+            {
+                switch (_mainMenuSelect)
+                {
+                case 0:
+                    _solveTimeTick = true;
+                    _state = GameState.Game;
+                    break;
+                case 1:
+                    Exit();
+                    break;
+                }
+            }
+            break;
+        }
 
         base.Update(gameTime);
     }
@@ -84,8 +125,18 @@ public class NonoSharpGame : Game
 
         _spriteBatch.Begin();
 
-        _board.Draw(_spriteBatch, GraphicsDevice);
-        TextRenderer.DrawText(_spriteBatch, "notosans", 10, 10, 0.6f, $"Time: {Math.Round(_solveTime / 1000, 2)} s", _board.IsSolved ? Color.Lime : Color.White);
+        switch (_state)
+        {
+        case GameState.Game:
+            _board.Draw(_spriteBatch, GraphicsDevice);
+            TextRenderer.DrawText(_spriteBatch, "notosans", 10, 10, 0.6f, $"Time: {Math.Round(_solveTime / 1000, 2)} s", _board.IsSolved ? Color.Lime : Color.White);
+            break;
+        case GameState.MainMenu:
+            TextRenderer.DrawText(_spriteBatch, "notosans", 10, 10, "nonoSharp", Color.White);
+            drawMainMenuButtons();
+            break;
+        }
+
         TextRenderer.DrawText(_spriteBatch, "notosans", 10, GraphicsDevice.Viewport.Bounds.Height - 26, 0.33f, $"{Math.Round(_fpsCounter.CurrentFPS)} fps, {Math.Round(_fpsCounter.AverageFPS)} avg", Color.LightGray);
 
         _spriteBatch.End();
@@ -93,6 +144,17 @@ public class NonoSharpGame : Game
         _fpsCounter.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
 
         base.Draw(gameTime);
+    }
+
+    private void drawMainMenuButtons()
+    {
+        for (int i = 0; i < _mainMenuButtons.Length; i++)
+        {
+            bool selected = _mainMenuSelect == i;
+            string buttonText = selected ? $"> {_mainMenuButtons[i]} <" : _mainMenuButtons[i];
+            Color color = selected ? Color.Lime : Color.White;
+            TextRenderer.DrawText(_spriteBatch, "notosans", 10, 100 + 29 * i, 0.5f, buttonText, color);
+        }
     }
 
     protected override void OnExiting(object sender, EventArgs e)
