6db30852d074576d1f78fe74c1f14ecb993ffb5d
IAmMoltony <koronavirusnyj@gmail.com>
2024-02-13 18:42:01 +0600

    Add release script

--- release.sh
+++ release.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
-dotnet publish --configuration Release --runtime linux-x64 --self-contained
-dotnet publish --configuration Release --runtime win7-x64 --self-contained
+set -e
+
+rm -rf linux-build windows-build
+
+echo "* Building for Linux x64 *"
+dotnet publish -v m --configuration Release --runtime linux-x64 --self-contained
+rm bin/Release/net6.0/linux-x64/publish -r
+mv bin/Release/net6.0/linux-x64 linux-build
+
+echo "* Building for Windows 7 x64 *"
+dotnet publish -v m --configuration Release --runtime win7-x64 --self-contained
+rm bin/Release/net6.0/win7-x64/publish -r
+mv bin/Release/net6.0/win7-x64 windows-build
+
+echo "* Zipping Linux build *"
+zip -r nonoSharpLinux.zip -xi linux-build/*
+
+echo "* Zipping Windows build *"
+zip -r nonoSharpWindows.zip -xi windows-build/*
