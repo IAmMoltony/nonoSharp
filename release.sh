c27b796eba337419c06bf5ec8b161ca93eb37ce1
IAmMoltony <koronavirusnyj@gmail.com>
2024-03-20 18:48:49 +0500

    Update release script

--- release.sh
+++ release.sh
@@ -2,21 +2,22 @@
 
 set -e
 
+echo " ** Cleaning up **"
 rm -rf linux-build windows-build
-dotnet clean
+dotnet clean -v m
 
-echo "* Building for Linux x64 *"
+echo " ** Building for Linux x64 **"
 dotnet publish -v m --configuration Release --runtime linux-x64 --self-contained
 rm bin/Release/net6.0/linux-x64/publish -r
 mv bin/Release/net6.0/linux-x64 linux-build
 
-echo "* Building for Windows 7 x64 *"
+echo " ** Building for Windows 7 x64 **"
 dotnet publish -v m --configuration Release --runtime win7-x64 --self-contained
 rm bin/Release/net6.0/win7-x64/publish -r
 mv bin/Release/net6.0/win7-x64 windows-build
 
-echo "* Zipping Linux build *"
+echo " ** Zipping Linux build **"
 zip -r nonoSharpLinux.zip -xi linux-build/*
 
-echo "* Zipping Windows build *"
+echo " ** Zipping Windows build **"
 zip -r nonoSharpWindows.zip -xi windows-build/*
