fe850b3257e006d4f72c67adfc941a319f4ed090
IAmMoltony <koronavirusnyj@gmail.com>
2024-04-06 12:37:21 +0500

    Add editor board reset

--- src/Editor/EditorMain.cs
+++ src/Editor/EditorMain.cs
@@ -8,23 +8,34 @@ namespace NonoSharp.Editor;
 public class EditorMain
 {
     public Button SaveButton { get; private set; }
+    public Button ResetButton { get; private set; }
     public EditorBoard Board { get; private set; }
 
     public EditorMain()
     {
         Board = new();
         SaveButton = new(10, 10, 0, 45, StringManager.GetString("save"), Color.DarkGreen, Color.Green, true);
+        ResetButton = new(10, 60, 0, 45, StringManager.GetString("reset"), Color.DarkGreen, Color.Green, true);
     }
 
     public void Update(MouseState mouse, MouseState mouseOld, KeyboardState kb, KeyboardState kbOld, GraphicsDevice graphDev)
     {
         Board.Update(mouse, mouseOld, graphDev);
         SaveButton.Update(mouse, mouseOld, kb, kbOld);
+        ResetButton.Update(mouse, mouseOld, kb, kbOld);
+
+        if (ResetButton.IsClicked)
+        {
+            int size = Board.size;
+            Board.Reset();
+            Board.Make(size);
+        }
     }
 
     public void Draw(SpriteBatch sprBatch)
     {
         Board.Draw(sprBatch);
         SaveButton.Draw(sprBatch);
+        ResetButton.Draw(sprBatch);
     }
 }
