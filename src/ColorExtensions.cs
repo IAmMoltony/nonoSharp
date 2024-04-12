bc35c73da07eebd14fb0f5e02b369e093ba2c165
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2024-04-12 21:28:46 +0500

    Add docs to ColorExtensions

--- src/ColorExtensions.cs
+++ src/ColorExtensions.cs
@@ -3,8 +3,17 @@ using System;
 
 namespace NonoSharp;
 
+/// <summary>
+/// A class for extensions of the <c>Microsoft.Xna.Framework.Color</c> class.
+/// </summary>
 public static class ColorExtensions
 {
+    /// <summary>
+    /// Get a darker version of a color.
+    /// </summary>
+    /// <param name="color">The color to get a dark version of</param>
+    /// <param name="howMuch">How much darker to make the color (0.0 to 1.0 inclusive)</param>
+    /// <returns>A darker version of the color</returns>
     public static Color Darker(this Color color, float howMuch)
     {
         // Make sure `howMuch' is 0.0 to 1.0
