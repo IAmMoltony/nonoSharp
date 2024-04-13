a68ecf5304a32419ff6cf5c06e540061f0ab318b
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2024-04-13 22:45:07 +0500

    Add docs to `StringManager`

--- src/StringManager.cs
+++ src/StringManager.cs
@@ -11,13 +11,26 @@ namespace NonoSharp;
 
 public static class StringManager
 {
+    /// <summary>
+    /// Dictionary of language strings
+    /// </summary>
     private static Dictionary<string, string> _strings;
+
+    /// <summary>
+    /// The current language
+    /// </summary>
     private static string _lang;
 
+    /// <summary>
+    /// List of supported languages
+    /// </summary>
     public static readonly string[] SupportedLanguages = {
         "en", "ru"
     };
 
+    /// <summary>
+    /// Initialize string manager
+    /// </summary>
     public static void Initialize()
     {
         Log.Logger.Information("Initializing string manager");
@@ -42,6 +55,11 @@ public static class StringManager
         Log.Logger.Information($"Initialized in {stopwatch.ElapsedMilliseconds} ms");
     }
 
+    /// <summary>
+    /// Get a language string
+    /// </summary>
+    /// <param name="key">String key</param>
+    /// <returns>The string translated to the current language</returns>
     public static string GetString(string key)
     {
         if (!_strings.ContainsKey(key))
@@ -49,6 +67,9 @@ public static class StringManager
         return _strings[key];
     }
 
+    /// <summary>
+    /// Parse a language file
+    /// </summary>
     private static void parseLanguage()
     {
         string fileName = $"{AppDomain.CurrentDomain.BaseDirectory}/Content/lang/{_lang}.json";
@@ -56,6 +77,9 @@ public static class StringManager
         _strings = JsonSerializer.Deserialize<Dictionary<string, string>>(langString);
     }
 
+    /// <summary>
+    /// Get the current language
+    /// </summary>
     private static void getLanguage()
     {
         string langSetting = Settings.Get("language");
