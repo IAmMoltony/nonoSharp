028aa16a8f11ec05351f2dc2972af24974a5d24f
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2024-04-13 22:40:22 +0500

    Add docs to `RectRenderer`

--- src/RectRenderer.cs
+++ src/RectRenderer.cs
@@ -6,8 +6,15 @@ namespace NonoSharp;
 
 public static class RectRenderer
 {
+    /// <summary>
+    /// 1x1 pixel texture used for rendering rectangles
+    /// </summary>
     private static Texture2D _texture;
 
+    /// <summary>
+    /// Load the rectangle renderer
+    /// </summary>
+    /// <param name="graphDev">Graphics device to use</param>
     public static void Load(GraphicsDevice graphDev)
     {
         Log.Logger.Information("Loading rect renderer");
@@ -15,11 +22,24 @@ public static class RectRenderer
         _texture.SetData(new[] { Color.White });
     }
 
+    /// <summary>
+    /// Draw a filled rectangle
+    /// </summary>
+    /// <param name="rect">Rectangle to draw</param>
+    /// <param name="color">Color of the rectangle</param>
+    /// <param name="batch">Sprite batch to use</param>
     public static void DrawRect(Rectangle rect, Color color, SpriteBatch batch)
     {
         batch.Draw(_texture, rect, color);
     }
 
+    /// <summary>
+    /// Draw a rectangle outline
+    /// </summary>
+    /// <param name="rect">Rectangle to draw</param>
+    /// <param name="color">Color of the outline</param>
+    /// <param name="thickness">How thick the outline should be (in pixels)</param>
+    /// <param name="batch">Sprite batch to use</param>
     public static void DrawRectOutline(Rectangle rect, Color color, int thickness, SpriteBatch batch)
     {
         DrawRect(new Rectangle(rect.X, rect.Y, thickness, rect.Height + thickness), color, batch);
@@ -28,6 +48,12 @@ public static class RectRenderer
         DrawRect(new Rectangle(rect.X, rect.Y + rect.Height, rect.Width + thickness, thickness), color, batch);
     }
 
+    /// <summary>
+    /// Draw a rectangle outline with 1 pixel thickness
+    /// </summary>
+    /// <param name="rect">Rectangle to draw</param>
+    /// <param name="color">Color of the outline</param>
+    /// <param name="sprBatch">Sprite batch to use</param>
     public static void DrawRectOutline(Rectangle rect, Color color, SpriteBatch sprBatch)
     {
         DrawRectOutline(rect, color, 1, sprBatch);
