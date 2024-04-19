0a985a1e278a9bb38a76360ee28a8191849a6e83
IAmMoltony <koronavirusnyj@gmail.com>
2024-04-19 23:13:34 +0500

    update release.sh

--- release.sh
+++ release.sh
@@ -7,24 +7,24 @@ if [ "$1" == "" ]; then
 	exit 1
 fi
 
-echo -e "  *** \033[0;32mCleaning up\033[0m **"
+echo -e "  *** \033[0;32mCleaning up\033[0m ***"
 rm -rf linux-build windows-build Content/bin Content/obj
 dotnet clean -v m
 
-echo -e "  *** \033[0;32mBuilding for \033[0;33mLinux x64\033[0m **"
+echo -e "  *** \033[0;32mBuilding for \033[0;33mLinux x64\033[0m ***"
 dotnet publish -v m --configuration Release --runtime linux-x64 --self-contained
 rm bin/Release/net6.0/linux-x64/publish -r
 mv bin/Release/net6.0/linux-x64 linux-build
 
-echo -e "  *** \033[0;32mBuilding for \033[0;33mWindows 7\033[0m x64 **"
+echo -e "  *** \033[0;32mBuilding for \033[0;33mWindows 7\033[0m x64 ***"
 dotnet publish -v m --configuration Release --runtime win7-x64 --self-contained
 rm bin/Release/net6.0/win7-x64/publish -r
 mv bin/Release/net6.0/win7-x64 windows-build
 
-echo -e "  *** \033[0;32mZipping Linux build\033[0m **"
+echo -e "  *** \033[0;32mZipping Linux build\033[0m ***"
 cd linux-build
 zip -r ../nonoSharpLinux$1.zip -xi ./*
 
-echo -e "  *** \033[0;32mZipping Windows build\033[0m **"
+echo -e "  *** \033[0;32mZipping Windows build\033[0m ***"
 cd ../windows-build
 zip -r ../nonoSharpWindows$1.zip -xi ./*
