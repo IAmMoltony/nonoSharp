96a2c23d56f2bc816cec13549b0a78dec6d7494f
IAmMoltony <koronavirusnyj@gmail.com>
2024-05-21 18:56:15 +0500

    fix parentheses

--- src/NonoSharp.cs
+++ src/NonoSharp.cs
@@ -299,7 +299,7 @@ public class NonoSharpGame : Game
         GridRenderer.DrawGrid(
                 _spriteBatch,
                 (int)((_mouse.X - (GraphicsDevice.Viewport.Bounds.Width / 2)) * 0.03f) - 100,
-                (int)(((_mouse.Y - GraphicsDevice.Viewport.Bounds.Height / 2)) * 0.03f) - 100,
+                (int)((_mouse.Y - (GraphicsDevice.Viewport.Bounds.Height / 2)) * 0.03f) - 100,
                 (GraphicsDevice.Viewport.Bounds.Height / 32) + 101,
                 (GraphicsDevice.Viewport.Bounds.Width / 32) + 101,
                 32,
