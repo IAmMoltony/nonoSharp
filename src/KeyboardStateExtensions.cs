9b04eceee60c5143eb50362f2c6e60d29275bbc7
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2024-06-01 22:47:20 +0500

    Make `KeyboardStateExtensions.IsCombinationPressed` more concise

--- src/KeyboardStateExtensions.cs
+++ src/KeyboardStateExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework.Input;
+using System.Linq;
 
 namespace NonoSharp;
 
@@ -6,24 +7,6 @@ public static class KeyboardStateExtensions
 {
     public static bool IsCombinationPressed(this KeyboardState state, KeyboardState oldState, params Keys[] keys)
     {
-        // all keys have to be pressed in the current state
-        foreach (var key in keys)
-        {
-            if (!state.IsKeyDown(key))
-            {
-                return false;
-            }
-        }
-
-        // at least one of the keys has to be pressed in the old state
-        foreach (var key in keys)
-        {
-            if (!oldState.IsKeyDown(key))
-            {
-                return true;
-            }
-        }
-
-        return false;
+        return keys.All(key => state.IsKeyDown(key)) && keys.Any(key => !oldState.IsKeyDown(key));
     }
 }
