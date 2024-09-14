882f9501c7e9eacd0e395a10a536f660f4377f3e
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2024-09-14 19:47:49 +0500

    fix №2

--- src/Editor/SaveLevelState.cs
+++ src/Editor/SaveLevelState.cs
@@ -84,7 +84,7 @@ public class SaveLevelState
 
     private void drawNameBox(SpriteBatch sprBatch)
     {
-        _levelNameBox.x = (sprBatch.GraphicsDevice.Viewport.Bounds.Width / 2) - (_levelNameBox.Width / 2);
+        _levelNameBox.x = (sprBatch.GraphicsDevice.Viewport.Bounds.Width / 2) - (_levelNameBox.width / 2);
         _levelNameBox.y = (sprBatch.GraphicsDevice.Viewport.Bounds.Height / 2) - (UI.TextBox.Height / 2);
         _levelNameBox.Draw(sprBatch);
     }
