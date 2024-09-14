e4c6bffbec7943439e4ef26a6d735faae058de17
Moltony <105192532+IAmMoltony@users.noreply.github.com>
2024-09-14 20:43:26 +0500

    Add no color switch to release script

--- release.ps1
+++ release.ps1
@@ -9,6 +9,8 @@
     Specify the version of the game. This only affects the file name of the zip files generated.
 .PARAMETER NoBuild
     Don't build the game solution.
+.PARAMETER NoColor
+    Disable colored output.
 .PARAMETER Verbosity
     The verbosity of .NET commands. Allowed values: q, m, n, d, diag. Default is 'm'.
 .EXAMPLE
@@ -18,6 +20,7 @@
 param (
     [Parameter(Mandatory=$true, ParameterSetName="GameVersion", Position=0)][string]$GameVersion,
     [switch]$NoBuild,
+    [switch]$NoColor,
     [Parameter(Mandatory=$false)][ValidateSet("q", "m", "n", "d", "diag")][string]$Verbosity = "m"
 )
 
@@ -30,7 +33,11 @@ if ($PSVersionTable.PSVersion.Major -lt 7) {
 }
 
 if (!$NoBuild) {
-    Write-Host "  *** `e[0;32mCleaning up`e[0m ***"
+    if ($NoColor) {
+        Write-Host "  *** Cleaning up ***"
+    } else {
+        Write-Host "  *** `e[0;32mCleaning up`e[0m ***"
+    }
 
     # remove all these folders (if they exist)
     $PathsToRemove = "./linux-build", "./windows-build", "./Content/bin", "./Content/obj"
@@ -44,22 +51,38 @@ if (!$NoBuild) {
     dotnet clean -v $Verbosity
 
     # build for linux
-    Write-Host "  *** `e[0;32mBuilding for `e[0;33mLinux x64`e[0m ***"
+    if ($NoColor) {
+        Write-Host "  *** Building for Linux x64 ***"
+    } else {
+        Write-Host "  *** `e[0;32mBuilding for `e[0;33mLinux x64`e[0m ***"
+    }
     dotnet publish -v $Verbosity --configuration Release --runtime linux-x64 --self-contained
     Move-Item ./bin/Release/net8.0/linux-x64/publish ./linux-build
 
     # build for windows
-    Write-Host "  *** `e[0;32mBuilding for `e[0;33mWindows x64`e[0m ***"
+    if ($NoColor) {
+        Write-Host "  *** Building for Windows x64 ***"
+    } else {
+        Write-Host "  *** `e[0;32mBuilding for `e[0;33mWindows x64`e[0m ***"
+    }
     dotnet publish -v $Verbosity --configuration Release --runtime win-x64 --self-contained
     Move-Item ./bin/Release/net8.0/win-x64/publish ./windows-build
 }
 
 # zip linux build
-Write-Host "  *** `e[0;32mZipping `e[0;33mLinux`e[0;32m build`e[0m ***"
+if ($NoColor) {
+    Write-Host "  *** Zipping Linux build ***"
+} else {
+    Write-Host "  *** `e[0;32mZipping `e[0;33mLinux`e[0;32m build`e[0m ***"
+}
 Compress-Archive -Path ./linux-build -DestinationPath ./nonoSharpLinux$GameVersion.zip -Force
 
 # zip windows build
-Write-Host "  *** `e[0;32mZipping `e[0;33mWindows`e[0;32m build`e[0m ***"
+if ($NoColor) {
+    Write-Host "  *** Zipping Windows build ***"
+} else {
+    Write-Host "  *** `e[0;32mZipping `e[0;33mWindows`e[0;32m build`e[0m ***"
+}
 Compress-Archive -Path ./windows-build -DestinationPath ./nonoSharpWindows$GameVersion.zip -Force
 
 # move builds to ReleaseBuilds/
@@ -67,4 +90,8 @@ New-Item -Path ./ReleaseBuilds -ItemType Directory -Force
 Move-Item nonoSharpLinux$GameVersion.zip ./ReleaseBuilds -Force
 Move-Item nonoSharpWindows$GameVersion.zip ./ReleaseBuilds -Force
 
-Write-Host "  *** `e[0;32mDone!`e[0m ***"
+if ($NoColor) {
+    Write-Host "  *** Done! ***"
+} else {
+    Write-Host "  *** `e[0;32mDone!`e[0m ***"
+}
