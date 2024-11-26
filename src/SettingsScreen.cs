6f1030aa93169261fbd768c37b24a6fd579d8cf2
IAmMoltony <koronavirusnyj@gmail.com>
2024-11-26 22:17:22 +0500

    Add accent color dialog (doesnt do anything right now)

--- src/SettingsScreen.cs
+++ src/SettingsScreen.cs
@@ -7,6 +7,9 @@ namespace NonoSharp;
 
 public class SettingsScreen : IGameState
 {
+    public const int DialogWidth = 600;
+    public const int DialogHeight = 280;
+
     public Button BackButton { get; private set; }
     public Button CreditsButton { get; private set; }
     public Button KeySettingsButton { get; private set; }
@@ -15,6 +18,10 @@ public class SettingsScreen : IGameState
     private readonly CheckBox _enableHintsBox;
     private readonly CheckBox _showBgBox;
     private readonly CheckBox _enableSoundBox;
+
+    // TODO reusable dialog code
+    private Rectangle _dialogRect;
+    private bool _setAccentColor;
 
     public SettingsScreen()
     {
@@ -27,6 +34,9 @@ public class SettingsScreen : IGameState
         _showBgBox = new(40, 140, StringManager.GetString("showBgGrid"), Color.Gray, Color.DarkGray, Settings.GetBool("showBgGrid"));
         _enableSoundBox = new(40, 170, StringManager.GetString("enableSound"), Color.Gray, Color.DarkGray, Settings.GetBool("sound"));
         AccentColorButton = new(40, 215, 0, 40, StringManager.GetString("changeAccentColor"), Settings.GetDarkAccentColor(), Settings.GetAccentColor(), true);
+
+        _dialogRect = new();
+        _setAccentColor = false;
     }
 
     public void Draw(SpriteBatch sprBatch)
@@ -43,32 +53,60 @@ public class SettingsScreen : IGameState
         _showBgBox.Draw(sprBatch);
         _enableSoundBox.Draw(sprBatch);
         AccentColorButton.Draw(sprBatch);
+
+        if (_setAccentColor)
+        {
+            Rectangle dialogTextRect = new();
+            
+            RectRenderer.DrawRect(new(0, 0, graphDev.Viewport.Bounds.Width, graphDev.Viewport.Bounds.Height), new(Color.Black, 0.5f), sprBatch);
+            dialogTextRect = _dialogRect;
+            dialogTextRect.Y -= 100;
+            RectRenderer.DrawRect(_dialogRect, Settings.GetDarkAccentColor(), sprBatch);
+            RectRenderer.DrawRectOutline(_dialogRect, Settings.GetAccentColor(), 2, sprBatch);
+
+            TextRenderer.DrawTextCenter(sprBatch, "DefaultFont", 0.8f, StringManager.GetString("changeAccentColor"), Color.White, dialogTextRect);
+        }
     }
 
     public IGameState? Update(MouseState mouse, MouseState mouseOld, KeyboardState kb, KeyboardState kbOld, GraphicsDevice graphDev, ref LevelMetadata levelMetadata, bool hasFocus)
     {
-        _enableHintsBox.Update(mouse, mouseOld, kb, kbOld);
-        _showBgBox.Update(mouse, mouseOld, kb, kbOld);
-        _enableSoundBox.Update(mouse, mouseOld, kb, kbOld);
-        AccentColorButton.Update(mouse, mouseOld, kb, kbOld);
-
-        BackButton.Update(mouse, mouseOld, kb, kbOld);
-        if (BackButton.IsClicked)
+        if (_setAccentColor)
         {
-            saveSettings();
-            return new MainMenu();
+            _dialogRect.X = (graphDev.Viewport.Bounds.Width / 2) - (_dialogRect.Width / 2);
+            _dialogRect.Y = (graphDev.Viewport.Bounds.Height / 2) - (DialogHeight / 2);
+            _dialogRect.Width = DialogWidth;
+            _dialogRect.Height = DialogHeight;
         }
+        else
+        {
+            _enableHintsBox.Update(mouse, mouseOld, kb, kbOld);
+            _showBgBox.Update(mouse, mouseOld, kb, kbOld);
+_enableSoundBox.Update(mouse, mouseOld, kb, kbOld);
+            AccentColorButton.Update(mouse, mouseOld, kb, kbOld);
 
-        CreditsButton.y = graphDev.Viewport.Bounds.Height - 50;
-        CreditsButton.Update(mouse, mouseOld, kb, kbOld);
-        if (CreditsButton.IsClicked)
-            return new Credits();
+            if (AccentColorButton.IsClicked)
+            {
+                _setAccentColor = true;
+            }
 
-        KeySettingsButton.y = CreditsButton.y;
-        KeySettingsButton.x = CreditsButton.x + CreditsButton.width + 10;
-        KeySettingsButton.Update(mouse, mouseOld, kb, kbOld);
-        if (KeySettingsButton.IsClicked)
-            return new KeySettingsScreen();
+            BackButton.Update(mouse, mouseOld, kb, kbOld);
+            if (BackButton.IsClicked)
+            {
+                saveSettings();
+                return new MainMenu();
+            }
+
+            CreditsButton.y = graphDev.Viewport.Bounds.Height - 50;
+            CreditsButton.Update(mouse, mouseOld, kb, kbOld);
+            if (CreditsButton.IsClicked)
+                return new Credits();
+
+            KeySettingsButton.y = CreditsButton.y;
+            KeySettingsButton.x = CreditsButton.x + CreditsButton.width + 10;
+            KeySettingsButton.Update(mouse, mouseOld, kb, kbOld);
+            if (KeySettingsButton.IsClicked)
+                return new KeySettingsScreen();
+        }
 
         return null;
     }
